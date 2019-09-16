b4eb83078cd695b786c8e33a754d3ea2153e6dbf
Chouaib <hamek.chouaib@gmail.com>
2019-09-16 10:09:02 +0200

    Add results show screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+    const [results, setResults] = useState(null);
+
     const id = navigation.getParam("id");
     const address = navigation.getParam("address");
     const name = navigation.getParam("name");
+
+    const getResult = async () => {
+        const response = await yelp(`/${id}`);
+        setResults(response.data);
+
+    }
+
+    useEffect(() => {
+        getResult();
+    }, []);
+
     return <View>
         <Text>Restaurant: {name}</Text>
         <Text>Address: {address}</Text>
         <Text>Results Show Screen</Text>
+        {(results) 
+        ? <FlatList 
+            data={results.photos}
+            keyExtractor={item => item}
+            renderItem={({ item }) => {
+                console.log("Rendering: ", item);
+                return <Image style={styles.image} source={{ uri: item }} />
+            }}
+        />
+        : null
+        }
+        {results? console.log(results.photos) : null}
     </View>
 };
 
 const styles = StyleSheet.create({
-
+    image: {
+        height: 200,
+        width: 300
+    }
 });
 
 export default ResultsShowScreen;
